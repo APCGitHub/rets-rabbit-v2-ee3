5abf6c2f5dccc1f7693b634eddb954a29f1b2a4e
Andrew Clinton <ddrew.clinton@gmail.com>
2017-08-16 16:01:32 -0400

    addon setup file

---
+++ system/user/addons/rets_rabbit_v2/addon.setup.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'author' => 'Anecka, LLC',
+    'author_url' => 'http://www.anecka.com',
+    'name' => 'Rets Rabbit V2',
+    'description' => 'Fetch MLS listings from the Rets Rabbit V2 API',
+    'version' => '1.0.0',
+    'namespace' => 'Anecka\RetsRabbit',
+    'settings_exist' => TRUE,
+    'docs_url' => 'https://retsrabbit.com/docs/v2'
+);
