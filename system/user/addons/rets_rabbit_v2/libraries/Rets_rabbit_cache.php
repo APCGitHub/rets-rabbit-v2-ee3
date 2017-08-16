834bf08ad8e9e75fde8be13d065ecaddfbce90bd
Andrew Clinton <ddrew.clinton@gmail.com>
2017-08-16 16:06:51 -0400

    rets rabbit cache lib

---
+++ system/user/addons/rets_rabbit_v2/libraries/Rets_rabbit_cache.php
@@ -0,0 +1,82 @@
+<?php
+
+class Rets_rabbit_cache
+{
+    /**
+     * Base path for all cache.
+     *
+     * @var string
+     */
+    private $basePath = '/rets_rabbit_v2/';
+
+    /**
+     * Rets_rabbit_cache constructor
+     */
+    public function __construct()
+    {
+        ee()->load->library('encrypt');
+    }
+
+    /**
+     * Set a value in the cache.
+     *
+     * @param  string  $key
+     * @param  boolean $isSecure
+     * @return bool
+     */
+    public function get($key, $isSecure = false)
+    {
+        $path = $this->basePath;
+
+        if($isSecure) {
+            $path .= 'secure/';
+        }
+
+        $path .= $key;
+        $value = ee()->cache->get($path);
+
+        if($isSecure && $value) {
+            $value = ee()->encrypt->decode($value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Set a key value in storage.
+     *
+     * @param string  $key
+     * @param mixed  $value
+     * @param integer $ttl
+     * @param boolean $secure
+     * @return bool
+     */
+    public function set($key, $value, $ttl = 3600, $secure = false)
+    {
+        $path = $this->basePath;
+
+        if($secure) {
+            $path .= 'secure/';
+            $value = ee()->encrypt->encode($value);
+        }
+
+        $path .= $key;
+
+        return ee()->cache->save($path, $value, $ttl);
+    }
+
+    /**
+     * Delete cache
+     *
+     * @param  string $key
+     * @return bool
+     */
+    public function delete($key = '')
+    {
+        $path = '/rets_rabbit/' . $key;
+
+        ee()->cache->delete($path);
+
+        return ee()->cache->clean();
+    }
+}
