3f81609b884d5765c0673906abede2cdb83511a1
Andrew Clinton <ddrew.clinton@gmail.com>
2017-08-16 16:03:22 -0400

    config file to define constants

---
+++ system/user/addons/rets_rabbit_v2/config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Rets_rabbit Module Class
+ *
+ * @package     rets_rabbit_v2
+ * @author      Andrew Clinton contact@anecka.com
+ * @copyright   Copyright (c) 2017, Andrew Clinton
+ * @link        http://retsrabbit.com
+ * @license		Creative Commons, Attribution-NoDerivatives 4.0
+ * 				http://creativecommons.org/licenses/by-nd/4.0/legalcode
+ */
+
+if ( ! defined('RETS_RABBIT_V2_NAME'))
+{
+	define('RETS_RABBIT_V2_NAME',    'Rets_rabbit_v2');
+	define('RETS_RABBIT_V2_PACKAGE', 'rets_rabbit_v2');
+	define('RETS_RABBIT_V2_VERSION', '1.0.0');
+}
