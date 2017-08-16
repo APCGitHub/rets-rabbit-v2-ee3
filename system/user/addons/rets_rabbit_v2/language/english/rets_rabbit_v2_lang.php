19abe4c25b130994604dd25241b7a581164dc2ab
Andrew Clinton <ddrew.clinton@gmail.com>
2017-08-16 16:06:34 -0400

    language file

---
+++ system/user/addons/rets_rabbit_v2/language/english/rets_rabbit_v2_lang.php
@@ -0,0 +1,65 @@
+<?php
+
+$lang = array(
+
+    /* ----------------------------------------
+    /*  Required for MODULES page
+    /* ----------------------------------------*/
+
+    "rets_rabbit_module_name"			=>    "Rets Rabbit",
+
+    "rets_rabbit_module_description" 	=>	   "RETS/MLS search add-on for ExpressionEngine, uses the Rets Rabbit API",
+
+    //-----------------------------------------//
+    "settings"                           =>    "Rets Rabbit Settings",
+
+    "settings_short"                    => "Settings",
+
+    "server"                             =>    "Servers",
+
+    "servers"                           =>      "Servers",
+
+    "metadata"                           =>    "Metadata",
+
+    "client_id"                          =>    "Client Id",
+
+    "client_secret"                      =>    "Client Secret",
+
+    "api_endpoint"                       =>    "Custom API Endpoint (optional)",
+
+    "short_code"                         =>    "Short Code",
+
+    "server_hash"                        =>    "Server Hash",
+
+    "is_default"                         =>    "Default",
+
+    "rets_no_search"                     =>    "No search parameters were submitted.",
+
+    "mls_id_required"                    =>    "The 'mls_id' parameter is required!",
+
+    "server_form_success"                =>    "Server settings have been updated!",
+
+    "config_form_success"                =>    "RETS configuration settings updated!",
+
+    "servers"                            =>    "Servers",
+
+    "select_a_server"                    =>    "Select a server from the drop down to view the metadata.",
+
+    "view_servers"                       =>    "Here are the servers registered for your account, if you're using multiple servers it's recommended you give each one an easy to remember 'short code'.",
+
+    "access_url"                         =>    "Access Url",
+
+    "server_name"                        =>    "Name",
+
+    "server_id"                          =>     "ID",
+
+    "api_error"                          =>    "There was an error accessing the API, make sure you're using the correct parameters in your tags.",
+
+    "refresh"                            =>    "Refresh from API",
+
+    "log_searches"                       =>    "Log searches",
+
+    /* END */
+    ''=>''
+);
+?>
