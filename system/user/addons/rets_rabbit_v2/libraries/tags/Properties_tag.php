5c30b01aea38c86fdbb017df1b740681d92da334
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-21 11:10:25 -0500

    extend new parent for cleaner code

--- system/user/addons/rets_rabbit_v2/libraries/tags/Properties_tag.php
+++ system/user/addons/rets_rabbit_v2/libraries/tags/Properties_tag.php
@@ -1,6 +1,8 @@
 <?php
 
-class Properties_tag
+require PATH_THIRD . "rets_rabbit_v2/libraries/tags/Base_tag.php";
+
+class Properties_tag extends Base_tag
 {
     /**
      * The different params this tag can handle
@@ -114,44 +116,5 @@ class Properties_tag
         }
 
         $this->params = $params;
-    }
-
-    /**
-     * Fetch the parameters as an associative array for the API
-     *
-     * @return array
-     */
-    public function toApiParams()
-    {
-        $data = array();
-
-        foreach($this->params as $k => $v) {
-            if(in_array($k, $this->apiParams)) {
-                if(in_array($k, array_keys($this->map))) {
-                    $k = $this->map[$k];
-                }
-
-                $data[$k] = $v;
-            }
-        }
-
-        return $data;
-    }
-
-    /**
-     * Return the param value requested
-     *
-     * @param  string $key
-     * @return mixed
-     */
-    public function __get($key)
-    {
-        if(in_array($key, array_keys($this->params))) {
-            return $this->params[$key];
-        } else if(in_array($key, array_keys($this->attrs))) {
-            return $this->attrs[$key];
-        }
-
-        return null;
     }
 }
